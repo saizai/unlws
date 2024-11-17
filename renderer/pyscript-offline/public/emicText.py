88f27f2297af3df4618d42ca2f2d346a685edd45
Alex Fink <000024@gmail.com>
2024-11-17 19:34:00 +0000

    build the text sentence in main

--- renderer/pyscript-offline/public/emicText.py
+++ renderer/pyscript-offline/public/emicText.py
@@ -1,11 +1,18 @@
-import math
 import xml.dom.minidom as minidom
 from glyphDictionary import SingleSVGGlyphDictionary
 
 class EmicText:
   """An UNLWS sentence, with the layout of how it sits on the page,"""
   
-  # TODO: implement this
+  def __init__(self):
+    self.glyphs = [] # glyphs in the sentence
+    
+    # FIXME: style does not belong in the dictionary
+    dictionary = SingleSVGGlyphDictionary('unlws_glyphs/glyphs.svg')
+    self.style = dictionary.style
+  
+  def add_glyph(self, glyph):
+    self.glyphs.append(glyph)
   
   def svg(self):
     """Return an SVG of this sentence as XML."""
@@ -13,28 +20,15 @@ class EmicText:
     # The operations herein should be refactored into
     # dictionary handling, etc.
     
-    dictionary = SingleSVGGlyphDictionary('unlws_glyphs/glyphs.svg')
-    
     svg = minidom.getDOMImplementation().createDocument("http://www.w3.org/2000/svg", "svg", None)
     svg.documentElement.setAttribute("xmlns", "http://www.w3.org/2000/svg")
     svg.documentElement.setAttribute("xmlns:unlws-renderer", "https://github.com/saizai/unlws")
     svg.documentElement.setAttribute("width", "192px")
     svg.documentElement.setAttribute("viewBox", "-3 -2 6 4")
     
-    svg.documentElement.appendChild(dictionary.style)
+    svg.documentElement.appendChild(self.style)
     
-    firstsg = dictionary.glyph_by_id("I")
-    firstsg.x = -2.
-    firstsg.y = 0.
-    firstsg.angle = -math.pi/2
-    #firstsg_elt.setAttribute("transform", "translate(-2 0) rotate(-90)")
-    svg.documentElement.appendChild(firstsg.svg())
-    
-    cat = dictionary.glyph_by_id("cat")
-    cat.x = 2.
-    cat.y = 0.
-    cat.angle = math.pi
-    #cat_elt.setAttribute("transform", "translate(2 0) rotate(180)")
-    svg.documentElement.appendChild(cat.svg())
+    for glyph in self.glyphs:
+      svg.documentElement.appendChild(glyph.svg())
     
     return svg
--- renderer/pyscript-offline/public/main.py
+++ renderer/pyscript-offline/public/main.py
@@ -1,11 +1,28 @@
+import math
 from pyscript import document
 document.body.append("Before importing... ")
 from canvas import HTMLDOMCanvas
 from emicText import EmicText
+from glyphDictionary import SingleSVGGlyphDictionary
 
 board = document.querySelector("#svgboard")
 canvas = HTMLDOMCanvas(board)
 text = EmicText() # currently has test logic in it
+
+dictionary = SingleSVGGlyphDictionary('unlws_glyphs/glyphs.svg')
+
+firstsg = dictionary.glyph_by_id("I")
+firstsg.x = -2.
+firstsg.y = 0.
+firstsg.angle = -math.pi/2
+text.add_glyph(firstsg)
+
+cat = dictionary.glyph_by_id("cat")
+cat.x = 2.
+cat.y = 0.
+cat.angle = math.pi
+text.add_glyph(cat)
+
 canvas.render(text)
 
 #e = document.createElementNS("http://www.w3.org/2000/svg", "use")
