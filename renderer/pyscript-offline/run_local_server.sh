824ab0464bbd2107f8faa7ab568fb9cf07cd3440
Sai <github@saizai.com>
2024-12-10 09:00:27 +0100

    Update run_local_server.sh to check python

--- renderer/pyscript-offline/run_local_server.sh
+++ renderer/pyscript-offline/run_local_server.sh
@@ -1,1 +1,8 @@
-python3 -m http.server -d ./public/
+if [ -x "$(command -v python3)" ]; then
+  python3 -m http.server -d ./public/
+elif [ -x "$(command -v python)" ]; then
+  python -m http.server -d ./public/
+else
+  echo "install python or python3"
+fi
+
