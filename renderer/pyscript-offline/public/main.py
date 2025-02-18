6c4a30561a1773ef580c381cf225e780d21a2550
jan Talisoso <38190993+Ists-Cilveks@users.noreply.github.com>
2025-02-19 00:12:04 +0200

    Bring back the example with different penalties
    This example shows the result of using different penalty functions in the relaxer.
    It was merged into master in commit 41d4aeadebee43d0f71f04f96e328353398acabd but I removed it while rebasing. Maybe I could've handled it better, but now it's a separate commit.

--- renderer/pyscript-offline/public/main.py
+++ renderer/pyscript-offline/public/main.py
@@ -14,40 +14,66 @@ class Main():
     self.dictionary = SingleSVGGlyphDictionary('unlws_glyphs/glyphs.svg')
 
 
-  def make_test_text(self):
+  def make_test_text(self, I_angle_offset = 0, distance_multiplier = 1):
     text = EmicText()
-    
+
     firstsg = self.dictionary.glyph_by_id("I")
     firstsg = relaxer.DifferentialGlyph.from_glyph(firstsg)
-    firstsg.x = -2.
+    firstsg.x = -2. * distance_multiplier
     firstsg.y = 0.
-    firstsg.angle = math.pi/6#-math.pi/2
+    firstsg.angle = -math.pi/2 + I_angle_offset#math.pi/6#-math.pi/2
     text.add_glyph(firstsg)
 
     cat = self.dictionary.glyph_by_id("cat")
     cat = relaxer.DifferentialGlyph.from_glyph(cat)
-    cat.x = 2.
+    cat.x = 2. * distance_multiplier
     cat.y = 0.
     cat.angle = math.pi
     text.add_glyph(cat)
 
     rel = RelLine(firstsg, "X", cat, "X")
     text.add_rel(rel)
-    
-    firstsg.dangle = 1.
 
+    cat2 = self.dictionary.glyph_by_id("cat")
+    cat2 = relaxer.DifferentialGlyph.from_glyph(cat2)
+    cat2.x = 2. * distance_multiplier
+    cat2.y = 1.
+    cat2.angle = math.pi
+    text.add_glyph(cat2)
+
+    rel2 = RelLine(firstsg, "X", cat2, "X")
+    text.add_rel(rel2)
+    
+    cat.dx = 1.
+    cat2.dx = 1.
+    
     return text
 
-  def render_with_comments(self, text):
+  def render_with_comments(self, text, description):
     canvas = self.append_canvas()
     canvas.render(text)
 
-    comment = ""
-    comment += f"Max curvatures: { [relaxer.curvature_penalty(rel) for rel in text.rels] }. "
-    comment += f"Derivatives: { [relaxer.deriv_curvature_penalty(rel) for rel in text.rels] }. "
+    comment = f"{description}:<br>"
+    comment += f"Curvature penalty: { relaxer.total_penalty(text, {"velocity": 0, "curvature": 1, "distance": 0})}.<br>"
+    comment += f"Curvature squared penalty: { relaxer.total_penalty(text, {"velocity": 0, "curvature": 0, "distance": 0, "curvature_squared": 1})}.<br>"
+    comment += f"Curvature penalty for top rel: { relaxer.curvature_penalty(text.rels[0])}.<br>"
+    comment += f"Curvature penalty for bottom rel: { relaxer.curvature_penalty(text.rels[1])}.<br>"
 
     self.append_text(canvas.parent, comment)
 
+
   def main(self):
-    text = self.make_test_text()
-    self.render_with_comments(text)
+    text = self.make_test_text(math.pi/6)
+    self.render_with_comments(text, "Initial")
+
+    velocity_relaxed_text = self.make_test_text(math.pi/6)
+    relaxer.relax(velocity_relaxed_text, penalty_coefficients={"velocity": 1, "curvature": 0})
+    self.render_with_comments(velocity_relaxed_text, "Relaxed velocity")
+
+    curvature_relaxed_text = self.make_test_text(math.pi/6)
+    relaxer.relax(curvature_relaxed_text, penalty_coefficients={"velocity": 0, "curvature": 5})
+    self.render_with_comments(curvature_relaxed_text, "Relaxed max curvature")
+
+    curvature_squared_relaxed_text = self.make_test_text(math.pi/6)
+    relaxer.relax(curvature_squared_relaxed_text, penalty_coefficients={"velocity": 0, "curvature": 0, "curvature_squared": 20})
+    self.render_with_comments(curvature_squared_relaxed_text, "Relaxed squares of max curvature")
